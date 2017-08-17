1f2861421d6d0ac2046606c7e2779fc43c1cd7e9
Cesar-Urteaga <Cesar-Urteaga@users.noreply.github.com>
2017-08-16 19:22:57 -0500

    Encapsulates the code that downloads the earthquake data into a function

--- R/get_data.R
+++ R/get_data.R
@@ -1,5 +1,15 @@
-#' Gets the latest data from the NOOA
-url <- "https://www.ngdc.noaa.gov/nndc/struts/results?bt_0=&st_0=&type_17=EXACT&query_17=None+Selected&op_12=eq&v_12=&type_12=Or&query_14=None+Selected&type_3=Like&query_3=&st_1=&bt_2=&st_2=&bt_1=&bt_4=&st_4=&bt_5=&st_5=&bt_6=&st_6=&bt_7=&st_7=&bt_8=&st_8=&bt_9=&st_9=&bt_10=&st_10=&type_11=Exact&query_11=&type_16=Exact&query_16=&bt_18=&st_18=&ge_19=&le_19=&type_20=Like&query_20=&display_look=189&t=101650&s=1&submit_all=Search+Database"
-temporal_file <- tempfile("NOOA", fileext = "txt")
-download.file(url, temporal_file)
-NOOA_raw_data <- readr::read_delim(temporal_file, delim = "\t")
+#' Obtains the latest earthquake data from the NOOA
+#'
+#' The function \code{download_earthquake_data} gets the most recent earthquake
+#' data from \href{https://www.ngdc.noaa.gov/nndc/struts/form?t=101650&s=1&d=1}{the NOOA's Webpage}.
+#'
+#' @return Returns a \href{https://blog.rstudio.org/2016/03/24/tibble-1-0-0/}{tibble}
+#' object.
+#' @export
+#' @importFrom readr read_delim
+download_earthquake_data <- function(data_frame_name) {
+  url <- "https://www.ngdc.noaa.gov/nndc/struts/results?bt_0=&st_0=&type_17=EXACT&query_17=None+Selected&op_12=eq&v_12=&type_12=Or&query_14=None+Selected&type_3=Like&query_3=&st_1=&bt_2=&st_2=&bt_1=&bt_4=&st_4=&bt_5=&st_5=&bt_6=&st_6=&bt_7=&st_7=&bt_8=&st_8=&bt_9=&st_9=&bt_10=&st_10=&type_11=Exact&query_11=&type_16=Exact&query_16=&bt_18=&st_18=&ge_19=&le_19=&type_20=Like&query_20=&display_look=189&t=101650&s=1&submit_all=Search+Database"
+  temporal_file <- tempfile("NOOA", fileext = "txt")
+  download.file(url, temporal_file)
+  readr::read_delim(temporal_file, delim = "\t")
+}
