33e39fe7c654cc165f18fb6f1547fbe8296f1c57
Cesar-Urteaga <Cesar-Urteaga@users.noreply.github.com>
2017-09-11 20:30:38 -0500

    Modifies the README file and adds an example to the theme_timeline function

--- R/theme-timeline.R
+++ R/theme-timeline.R
@@ -6,6 +6,34 @@
 #' @usage NULL
 #' @export
 #' @importFrom ggplot2 theme_minimal theme element_line element_blank
+#' @examples
+#' require(dplyr)
+#' require(ggplot2)
+#'
+#' # Before using the geom, we need to tidy the data up.
+#' raw_data <- get_earthquake_data()
+#' clean_data <- eq_clean_data(raw_data)
+#' clean_data <- eq_location_clean(clean_data)
+#'
+#' # Quakes in Indonesia
+#' timeline_plot <- clean_data %>%
+#'   filter(COUNTRY == "INDONESIA",
+#'          !is.na(EQ_PRIMARY),
+#'          YEAR %in% 2000:2016) %>%
+#'   ggplot(mapping = aes(x = DATE,
+#'                        size = EQ_PRIMARY,
+#'                        color = TOTAL_DEATHS / 1000,
+#'                        label = LOCATION_NAME)
+#'          ) +
+#'   geom_timeline() +
+#'   geom_timeline_label(line_height = 1 / 4) +
+#'   labs(size = "Richter scale value",
+#'        color = "# deaths in thousands",
+#'        y = "")
+#' # Sans the new theme.
+#'   timeline_plot
+#' # With the timeline's theme.
+#'   timeline_plot + theme_timeline()
 theme_timeline <- function(){
   ggplot2::theme_minimal() +
   ggplot2::theme(legend.position = "bottom",
