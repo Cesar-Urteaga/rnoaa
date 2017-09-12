ee70f79674b1a0b7a302aa9d811b030663d7bcab
Cesar-Urteaga <Cesar-Urteaga@users.noreply.github.com>
2017-09-12 01:00:21 -0500

    Adds the test files

---
+++ tests/testthat.R
@@ -0,0 +1,5 @@
+# File to run the battery of tests.
+library(testthat)
+library(rnoaa)
+
+test_check("rnoaa")
---
+++ tests/testthat/test-data-functions.R
@@ -0,0 +1,24 @@
+context("data functions")
+
+test_that("class is correct", {
+  # We expect that downloaded data is a tibble or data frame object and is of
+  # the same class without regard whether the data was downloaded or loaded.
+  downloaded_data <- download_earthquake_data()
+  loaded_data <- get_earthquake_data()
+
+  # Also, we hope that the class will not change after the processing has
+  # carried out.
+  clean_data <- eq_clean_data(loaded_data)
+  clean_location_data <- eq_location_clean(loaded_data)
+
+  expect_s3_class(downloaded_data, c("tbl_df", "tbl", "data.frame"))
+  expect_identical(class(downloaded_data), class(loaded_data))
+  expect_s3_class(clean_data, c("tbl_df", "tbl", "data.frame"))
+  expect_s3_class(clean_location_data, c("tbl_df", "tbl", "data.frame"))
+  }
+  )
+
+# test_that("All the required variables are present", {
+#     required_variables <- c("DAY", "MONTH", "YEAR")
+#   }
+#   )
